84e8c9baabd2afb6720ee689f8beb0c3bfb8bb29
John Helmert III <ajak@gentoo.org>
2022-08-01 21:06:23 -0500

    First commit
    Signed-off-by: John Helmert III <ajak@gentoo.org>

---
+++ podman-gentoo.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+if [[ -z ${1} ]]; then
+    echo "Needs an argument"
+    exit 1
+fi
+
+jobs=$(($(nproc)/2))
+
+DOCKERFILE="\
+FROM FROMIMAGE
+
+ENV FEATURES \"binpkg-multi-instance\"
+
+RUN ln -srf /etc/portage/world /var/lib/portage/world
+RUN mkdir -p /etc/portage/package.unmask
+RUN mkdir -p /etc/portage/env
+RUN mkdir -p /etc/portage/package.env
+
+ENV MAKEOPTS=\"-j${jobs} -l${jobs}\"
+RUN emerge -vuDN @world --with-bdeps=y --quiet=y --buildpkg --usepkg
+RUN perl-cleaner --all
+RUN emerge --depclean
+RUN emerge @preserved-rebuild
+RUN eselect news read >/dev/null
+
+CMD \"/bin/bash\""
+
+PODMAN_ARGS="--cap-add=CAP_SYS_ADMIN,CAP_NET_ADMIN,CAP_SYS_PTRACE
+--volume ${1}/:/etc/portage
+--volume /var/cache/distfiles:/var/cache/distfiles
+--volume /var/db/repos:/var/db/repos
+--volume ${2}:/var/cache/binpkgs"
+
+target="gentoo/stage3:systemd"
+
+if [[ -n $(podman images localhost/${1} -q) ]] 2>&1 >/dev/null; then
+    echo "Rebuilding ${1}"
+    target="localhost/${1}"
+else
+    echo "Building ${1}"
+fi
+
+echo "${DOCKERFILE}" | sed "s FROMIMAGE ${target} " | podman build --squash-all ${PODMAN_ARGS} --tag "localhost/${1}" -f - || exit
+
+podman image prune -f
+podman push --tls-verify=false "localhost/${1}" "${3}/${1}:latest"
